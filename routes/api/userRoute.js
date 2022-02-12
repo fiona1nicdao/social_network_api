6792cd9ae4d3648efa45e77ce11de58dd131e48a
FionaN <fiona.nicdao@northwestern.edu>
2022-02-12 09:16:06 -0600

    update userRoutes

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,2 +1,29 @@
 const router = require('express').Router();
 
+const{
+    getAllUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/userController');
+
+// endpoint: /api/users/
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+// endpoint: /api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+// endpoint: /api/users/:userId/friend
+router.route('/:userId/friend')
+    .get(addFriend);
+// endpoint: /api/users/:userId/friend/:friendId
+router.route('/:userId/friend/:friendId')
+    .delete(removeFriend);
+
+module.exports = router;
