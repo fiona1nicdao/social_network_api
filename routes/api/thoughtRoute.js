620c1e1ae1c5760cbf3c2d3fda2aabf50a4cdc6e
FionaN <fiona.nicdao@northwestern.edu>
2022-02-12 09:23:37 -0600

    update thoughtRoute

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const{
+    getAllThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+
+// endpoint: /api/thoughts/
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+router.route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router.route('/:thoughtId/reaction')
+    .get(addReaction)
+
+router.route('/:thoughtId/reaction/:reactionId')
+    .delete(removeReaction);
+
+module.exports = router;
