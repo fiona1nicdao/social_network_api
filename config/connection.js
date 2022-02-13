04cbc1008c1f80a6e323df9ed30a28b789114b33
FionaN <fiona.nicdao@northwestern.edu>
2022-02-12 23:16:03 -0600

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
-const {connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
+// const connectionString = process.env.MONGODB_URI
 
-const connectionString = process.env.MONGODB_URI
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api',{
+   useNewUrlParser: true,
+   useUnifiedTopology:true,
+})
 
-connect(connectionString,{
-    useNewUrlParser: true,
-    useUnifiedTopology:true,
-});
-
-module.exports = connection;
+module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 // what does this do?
 const cwd = process.cwd();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 const app = express();
 
 app.use(express.urlencoded({extended:true}));
