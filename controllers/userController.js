a2983087da04d3b52bbd8d9643b504860ca34822
FionaN <fiona.nicdao@northwestern.edu>
2022-02-12 21:01:49 -0600

    add updateUser function in User controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,6 +34,22 @@ module.exports = {
             .catch((err) =>res.status(500).json(err));
     },
     // updateUser
+    updateUser(req,res){
+        User.findOneAndUpdate(
+            { _id:req.params.userId},
+            {$set:req.body},
+            // DO i need this? {runValidators:true, new:true}
+        )
+        .then((user)=>
+            !user
+                ? res.status(404).json({message:'No user with this id!'})
+                : res.json(user)
+        )
+        .catch((err)=> {
+            console.log(err)
+            res.status(500).json(err)
+        })
+    },
     // delete a User
     deleteUser(req,res){
         User.findOneAndRemove({ _id:req.params.userId})
